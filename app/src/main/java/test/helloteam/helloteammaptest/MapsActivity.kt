32aa25a8659ba10358bc2a5a75a03a5d4bd5fa83
monamoe <arkhan2013@hotmail.com>
2021-02-22 17:48:32 -0500

    Map fragment can now be placed inside constraintlayout, allowing for buttons and other UI elements to be placed on the page

--- app/src/main/java/test/helloteam/helloteammaptest/MapsActivity.kt
+++ app/src/main/java/test/helloteam/helloteammaptest/MapsActivity.kt
@@ -1,18 +1,59 @@
 package test.helloteam.helloteammaptest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
-
+import android.os.Looper
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import android.Manifest;
+import android.content.Context;
+import android.location.Location;
+import android.provider.Settings;
+import androidx.annotation.NonNull;
+import java.util.concurrent.TimeUnit
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+
+    //permission integer
+    private val MY_PERMISSION_FINE_LOCATION: Int = 44
+
+
+    //for users location
+    // FusedLocationProviderClient - Main class for receiving location updates.
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    // LocationRequest - Requirements for the location updates, i.e., how often you
+// should receive updates, the priority, etc.
+    private lateinit var locationRequest: LocationRequest
+
+    // LocationCallback - Called when FusedLocationProviderClient has a new Location.
+    private lateinit var locationCallback: LocationCallback
+
+    // Used only for local storage of the last known location. Usually, this would be saved to your
+// database, but because this is a simplified sample without a full database, we only need the
+// last location to create a Notification if the user navigates away from the app.
+    private var currentLocation: Location? = null
+
+    var userLocationLon = 0.1;
+    var userLocationLat = 0.1;
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         //map shit
@@ -23,26 +64,90 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        //getting user location
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
+        if ( ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            fusedLocationProviderClient!!.lastLocation.addOnSuccessListener {
+                //program has permission
+                location->
+                if(location != null){
+                    //update user interface
 
+                }
+            }
+        }
+        else{
+            requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), MY_PERMISSION_FINE_LOCATION)
+        }
+
+        //idk
+        //for timing
+//        locationRequest = LocationRequest().apply {
+//            // Sets the desired interval for active location updates. This interval is inexact. You
+//            // may not receive updates at all if no location sources are available, or you may
+//            // receive them less frequently than requested. You may also receive updates more
+//            // frequently than requested if other applications are requesting location at a more
+//            // frequent interval.
+//            //
+//            // IMPORTANT NOTE: Apps running on Android 8.0 and higher devices (regardless of
+//            // targetSdkVersion) may receive updates less frequently than this interval when the app
+//            // is no longer in the foreground.
+//            interval = TimeUnit.SECONDS.toMillis(60)
+//
+//            // Sets the fastest rate for active location updates. This interval is exact, and your
+//            // application will never receive updates more frequently than this value.
+//            fastestInterval = TimeUnit.SECONDS.toMillis(30)
+//
+//            // Sets the maximum time when batched location updates are delivered. Updates may be
+//            // delivered sooner than this interval.
+//            maxWaitTime = TimeUnit.MINUTES.toMillis(2)
+//
+//            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+//        }
 
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray){
+        super.onRequestPermissionsResult(requestCode, permissions,grantResults)
+        when(requestCode){
+            MY_PERMISSION_FINE_LOCATION ->
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //permission granted
+                }
+                else{
+                    //permission denited
+                    Toast.makeText(applicationContext, "App requires location permission to be granted", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+        }
+    }
+
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * Manipulates the map once available
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(43.5890, 79.6441)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        //adding markers
+        //find a way to customize the marker icon
+
+        val userlocation = LatLng(userLocationLon, userLocationLat)
+        val toronto = LatLng(43.6532, 79.3832)
+        val mississauga = LatLng(79.6441, 43.6532)
+
+        val sydney = LatLng(-34.0, 151.0)
+
+        mMap.addMarker(MarkerOptions().position(toronto).title("Toronto"))
+        mMap.addMarker(MarkerOptions().position(sydney).title("Sydney"))
+        mMap.addMarker(MarkerOptions().position(mississauga).title("Mississauga"))
+
+
+        mMap.addMarker(MarkerOptions().position(userlocation).title("User Location"))
     }
+
+
+
+
+
 }
--- app/src/main/java/test/helloteam/helloteammaptest/test.kt
+++ app/src/main/java/test/helloteam/helloteammaptest/test.kt
@@ -1,166 +1,166 @@
-package test.helloteam.helloteammaptest
-
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.LocationManager
-import android.os.Bundle
-import android.os.Looper
-import android.provider.Settings
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.*
-
-class test : AppCompatActivity() {
-    // initializing
-    // FusedLocationProviderClient
-    // object
-    var mFusedLocationClient: FusedLocationProviderClient? = null
-
-    // Initializing other items
-    // from layout file
-    var PERMISSION_ID = 44
-    var userLocationLon = 0.1
-    var userLocationLat = 0.1
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
-        // method to get the location
-        lastLocation
-    }// if permissions aren't available,
-    // request for permissions
-// getting last
-    // location from
-    // FusedLocationClient
-    // object
-// check if location is enabled
-
-    // check if permissions are given
-    @get:SuppressLint("MissingPermission")
-    private val lastLocation: Unit
-        private get() {
-            // check if permissions are given
-            if (checkPermissions()) {
-
-                // check if location is enabled
-                if (isLocationEnabled) {
-
-                    // getting last
-                    // location from
-                    // FusedLocationClient
-                    // object
-                    mFusedLocationClient!!.lastLocation
-                        .addOnCompleteListener { task ->
-                            val location = task.result
-                            if (location == null) {
-                                requestNewLocationData()
-                            } else {
-                                userLocationLat = location.latitude
-                                userLocationLon = location.longitude
-                            }
-                        }
-                } else {
-                    Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG)
-                        .show()
-                    val intent =
-                        Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-                    startActivity(intent)
-                }
-            } else {
-                // if permissions aren't available,
-                // request for permissions
-                requestPermissions()
-            }
-        }
-
-    @SuppressLint("MissingPermission")
-    private fun requestNewLocationData() {
-
-        // Initializing LocationRequest
-        // object with appropriate methods
-        val mLocationRequest = LocationRequest()
-        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        mLocationRequest.interval = 5
-        mLocationRequest.fastestInterval = 0
-        mLocationRequest.numUpdates = 1
-
-        // setting LocationRequest
-        // on FusedLocationClient
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        mFusedLocationClient.requestLocationUpdates(
-            mLocationRequest,
-            mLocationCallback,
-            Looper.myLooper()
-        )
-    }
-
-    private val mLocationCallback: LocationCallback = object : LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult) {
-            val mLastLocation = locationResult.lastLocation
-            userLocationLat = mLastLocation.latitude
-            userLocationLon = mLastLocation.longitude
-        }
-    }
-
-    // method to check for permissions
-    private fun checkPermissions(): Boolean {
-        return ActivityCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-
-        // If we want background location
-        // on Android 10.0 and higher,
-        // use:
-        // ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
-    }
-
-    // method to request for permissions
-    private fun requestPermissions() {
-        ActivityCompat.requestPermissions(
-            this, arrayOf(
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ), PERMISSION_ID
-        )
-    }
-
-    // method to check
-    // if location is enabled
-    private val isLocationEnabled: Boolean
-        private get() {
-            val locationManager =
-                getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
-                LocationManager.NETWORK_PROVIDER
-            )
-        }
-
-    // If everything is alright then
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == PERMISSION_ID) {
-            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                lastLocation
-            }
-        }
-    }
-
-    public override fun onResume() {
-        super.onResume()
-        if (checkPermissions()) {
-            lastLocation
-        }
-    }
-}
+//package test.helloteam.helloteammaptest
+//
+//import android.Manifest
+//import android.annotation.SuppressLint
+//import android.content.Context
+//import android.content.Intent
+//import android.content.pm.PackageManager
+//import android.location.LocationManager
+//import android.os.Bundle
+//import android.os.Looper
+//import android.provider.Settings
+//import android.widget.Toast
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.core.app.ActivityCompat
+//import com.google.android.gms.location.*
+//
+//class test : AppCompatActivity() {
+//    // initializing
+//    // FusedLocationProviderClient
+//    // object
+//    var mFusedLocationClient: FusedLocationProviderClient? = null
+//
+//    // Initializing other items
+//    // from layout file
+//    var PERMISSION_ID = 44
+//    var userLocationLon = 0.1
+//    var userLocationLat = 0.1
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+//
+//        // method to get the location
+//        lastLocation
+//    }// if permissions aren't available,
+//    // request for permissions
+//// getting last
+//    // location from
+//    // FusedLocationClient
+//    // object
+//// check if location is enabled
+//
+//    // check if permissions are given
+//    @get:SuppressLint("MissingPermission")
+//    private val lastLocation: Unit
+//        private get() {
+//            // check if permissions are given
+//            if (checkPermissions()) {
+//
+//                // check if location is enabled
+//                if (isLocationEnabled) {
+//
+//                    // getting last
+//                    // location from
+//                    // FusedLocationClient
+//                    // object
+//                    mFusedLocationClient!!.lastLocation
+//                        .addOnCompleteListener { task ->
+//                            val location = task.result
+//                            if (location == null) {
+//                                requestNewLocationData()
+//                            } else {
+//                                userLocationLat = location.latitude
+//                                userLocationLon = location.longitude
+//                            }
+//                        }
+//                } else {
+//                    Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG)
+//                        .show()
+//                    val intent =
+//                        Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+//                    startActivity(intent)
+//                }
+//            } else {
+//                // if permissions aren't available,
+//                // request for permissions
+//                requestPermissions()
+//            }
+//        }
+//
+//    @SuppressLint("MissingPermission")
+//    private fun requestNewLocationData() {
+//
+//        // Initializing LocationRequest
+//        // object with appropriate methods
+//        val mLocationRequest = LocationRequest()
+//        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+//        mLocationRequest.interval = 5
+//        mLocationRequest.fastestInterval = 0
+//        mLocationRequest.numUpdates = 1
+//
+//        // setting LocationRequest
+//        // on FusedLocationClient
+//        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+//        mFusedLocationClient.requestLocationUpdates(
+//            mLocationRequest,
+//            mLocationCallback,
+//            Looper.myLooper()
+//        )
+//    }
+//
+//    private val mLocationCallback: LocationCallback = object : LocationCallback() {
+//        override fun onLocationResult(locationResult: LocationResult) {
+//            val mLastLocation = locationResult.lastLocation
+//            userLocationLat = mLastLocation.latitude
+//            userLocationLon = mLastLocation.longitude
+//        }
+//    }
+//
+//    // method to check for permissions
+//    private fun checkPermissions(): Boolean {
+//        return ActivityCompat.checkSelfPermission(
+//            this,
+//            Manifest.permission.ACCESS_COARSE_LOCATION
+//        ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+//            this,
+//            Manifest.permission.ACCESS_FINE_LOCATION
+//        ) == PackageManager.PERMISSION_GRANTED
+//
+//        // If we want background location
+//        // on Android 10.0 and higher,
+//        // use:
+//        // ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+//    }
+//
+//    // method to request for permissions
+//    private fun requestPermissions() {
+//        ActivityCompat.requestPermissions(
+//            this, arrayOf(
+//                Manifest.permission.ACCESS_COARSE_LOCATION,
+//                Manifest.permission.ACCESS_FINE_LOCATION
+//            ), PERMISSION_ID
+//        )
+//    }
+//
+//    // method to check
+//    // if location is enabled
+//    private val isLocationEnabled: Boolean
+//        private get() {
+//            val locationManager =
+//                getSystemService(Context.LOCATION_SERVICE) as LocationManager
+//            return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+//                LocationManager.NETWORK_PROVIDER
+//            )
+//        }
+//
+//    // If everything is alright then
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        if (requestCode == PERMISSION_ID) {
+//            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                lastLocation
+//            }
+//        }
+//    }
+//
+//    public override fun onResume() {
+//        super.onResume()
+//        if (checkPermissions()) {
+//            lastLocation
+//        }
+//    }
+//}
